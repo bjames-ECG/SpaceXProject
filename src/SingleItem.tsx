02eafaf7ad47e1f907f29ddb5dc863b0f5049aad
Brendon James <brendonwjames@Brendons-MacBook-Pro.local>
2024-02-07 15:12:55 -0800

    type additions

--- src/SingleItem.tsx
+++ src/SingleItem.tsx
@@ -3,70 +3,60 @@ import "./SingleItem.css";
 import { Data } from './App';
 
 interface Props {
-  proppedSingleLaunch: Data[];
+  proppedSingleLaunch: Data | null; // Change type to Data | null
   addFavorite: (missionName: string) => void;
   favorites: Data[];
 }
 
-
-
-function SingleItem({ proppedSingleLaunch, addFavorite, favorites } : Props) {
-
+function SingleItem({ proppedSingleLaunch, addFavorite, favorites }: Props) {
   const [loading, setLoading] = useState<boolean>(false);
-
-  // console.log("PROPPED: ", proppedSingleLaunch)
 
   useEffect(() => {
     launchData();
-}, [])
+  }, []);
 
-const launchData = () => {
-  try {
-    setLoading(true);
-    setLoading(false);
-  } catch (error) {
-    setLoading(false);
+  const launchData = () => {
+    try {
+      setLoading(true);
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+    }
   }
-}
 
-const dateConverter = (utcDate : string) => {
-  // console.log(new Date(utcDate))
-  return new Date(utcDate).toDateString()
-}
-
-
+  const dateConverter = (utcDate: string) => {
+    return new Date(utcDate).toDateString();
+  }
 
   if (loading) {
     return <>Loading. . .</>
   }
 
-
-  if (!proppedSingleLaunch.mission_name) {
+  if (!proppedSingleLaunch) { // Check if proppedSingleLaunch exists
     return <div id="outer-container">Select a launch</div>
   }
 
-    return (
-      <>
-        <div id="outer-container">
+  return (
+    <>
+      <div id="outer-container">
         <div id='single-item-individual-launch' key={proppedSingleLaunch.mission_name}>
-        {(!favorites.filter(data => data.mission_name === proppedSingleLaunch.mission_name).length) && <button onClick={() => addFavorite(proppedSingleLaunch.mission_name)}>Add Favorite</button>}
-            <div><span id="label-text-font">Flight Number</span>: {proppedSingleLaunch.flight_number}</div>
-            <div><span id="label-text-font">Mission Name</span>: {proppedSingleLaunch.mission_name}</div>
-            <div><span id="label-text-font">Flight Details</span>: {proppedSingleLaunch.details}</div>
-            <div><span id="label-text-font">Launch Date</span>: {dateConverter(proppedSingleLaunch.launch_date_local)}</div>
-            {/* <div><span id="label-text-font">Launch Date</span>: {proppedSingleLaunch.launch_date_local}</div> */}
-            {proppedSingleLaunch.launch_success === true && <div id="label-text-font">Launch was successful</div>}
-            {proppedSingleLaunch.launch_success === false && 
+          {(!favorites.some(data => data.mission_name === proppedSingleLaunch.mission_name)) && <button onClick={() => addFavorite(proppedSingleLaunch.mission_name)}>Add Favorite</button>}
+          <div><span id="label-text-font">Flight Number</span>: {proppedSingleLaunch.flight_number}</div>
+          <div><span id="label-text-font">Mission Name</span>: {proppedSingleLaunch.mission_name}</div>
+          <div><span id="label-text-font">Flight Details</span>: {proppedSingleLaunch.details}</div>
+          <div><span id="label-text-font">Launch Date</span>: {dateConverter(proppedSingleLaunch.launch_date_local)}</div>
+          {proppedSingleLaunch.launch_success === true && <div id="label-text-font">Launch was successful</div>}
+          {proppedSingleLaunch.launch_success === false &&
             <div>
               <span id="label-text-font">Launch Failure Details</span>
-            {proppedSingleLaunch.launch_success === false && <div><span id="label-text-font">Reason</span>: {proppedSingleLaunch.launch_failure_details.reason}</div>}
-            {proppedSingleLaunch.launch_success === false && <div><span id="label-text-font">Altitude</span>: {proppedSingleLaunch.launch_failure_details.altitude}</div>}
-            {proppedSingleLaunch.launch_success === false && <div><span id="label-text-font">Time</span>: {proppedSingleLaunch.launch_failure_details.time}</div>}
-              </div>}
+              <div><span id="label-text-font">Reason</span>: {proppedSingleLaunch.launch_failure_details?.reason}</div>
+              <div><span id="label-text-font">Altitude</span>: {proppedSingleLaunch.launch_failure_details?.altitude}</div>
+              <div><span id="label-text-font">Time</span>: {proppedSingleLaunch.launch_failure_details?.time}</div>
+            </div>}
         </div>
-        </div>
-      </>
-    )
-  }
+      </div>
+    </>
+  )
+}
 
-  export default SingleItem
+export default SingleItem;
