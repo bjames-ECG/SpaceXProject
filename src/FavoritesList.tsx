f173dc5b715d99cff56141ae2d1b8654c238cba9
Brendon James <brendonwjames@Brendons-MacBook-Pro.local>
2024-02-07 14:53:22 -0800

    more type additions

--- src/FavoritesList.tsx
+++ src/FavoritesList.tsx
@@ -1,7 +1,28 @@
 import { useEffect } from 'react';
 import "./FavoritesList.css";
 
-function FavoritesList({ favorites, removeFavorite }) {
+interface Data {
+  mission_name: string;
+  flight_number: number;
+  details: string;
+  launch_date_local: string;
+  launch_site: {
+    site_name: string;
+  };
+  launch_success: boolean;
+  launch_failure_details?: {
+    reason: string;
+    altitude: number;
+    time: number;
+  };
+}
+
+interface Props {
+  favorites: Data[];
+  removeFavorite: (missionName: string) => void;
+}
+
+function FavoritesList({ favorites, removeFavorite }: Props) {
 
   useEffect(() => {
     if (favorites.length > 0) localStorage.setItem("Favorites", JSON.stringify(favorites));
@@ -9,38 +30,38 @@ function FavoritesList({ favorites, removeFavorite }) {
   }, [favorites]);
 
   //function used to convert the date format from the fetched utc version to a more readable format
-  const dateConverter = (utcDate) => {
+  const dateConverter = (utcDate: string) => {
     // console.log(new Date(utcDate))
     return new Date(utcDate).toDateString()
   }
 
-    return (
-      <>
-        <div id="outer-container">
-          Saved Favorites
+  return (
+    <>
+      <div id="outer-container">
+        Saved Favorites
         <div id="all-launches-box">
-          {favorites && favorites.map((data : any) => (
-                <div id='favorites-individual-launch' key={data.mission_name}>
-                    <button onClick={() => removeFavorite(data.mission_name)}>Remove Favorite</button>
-                    <div><span id="label-text-font">Flight Number</span>: {data.flight_number}</div>
-                    <div><span id="label-text-font">Mission Name</span>: {data.mission_name}</div>
-                    <div><span id="label-text-font">Flight Details</span>: {data.details}</div>
-                    <div><span id="label-text-font">Launch Date</span>: {dateConverter(data.launch_date_local)}</div>
-                    <div><span id="label-text-font">Launch Site Name</span>: {data.launch_site.site_name}</div>
-                    {data.launch_success === true && <div id="label-text-font">Launch was successful</div>}
-                    {data.launch_success === false && 
-                    <div>
-                      <span id="label-text-font">Launch Failure Details</span>
-                    {data.launch_success === false && <div><span id="label-text-font">Reason</span>: {data.launch_failure_details.reason}</div>}
-                    {data.launch_success === false && <div><span id="label-text-font">Altitude</span>: {data.launch_failure_details.altitude}</div>}
-                    {data.launch_success === false && <div><span id="label-text-font">Time</span>: {data.launch_failure_details.time}</div>}
-                      </div>}
-                </div>
-            ))}
+          {favorites && favorites.map((data) => (
+            <div id='favorites-individual-launch' key={data.mission_name}>
+              <button onClick={() => removeFavorite(data.mission_name)}>Remove Favorite</button>
+              <div><span id="label-text-font">Flight Number</span>: {data.flight_number}</div>
+              <div><span id="label-text-font">Mission Name</span>: {data.mission_name}</div>
+              <div><span id="label-text-font">Flight Details</span>: {data.details}</div>
+              <div><span id="label-text-font">Launch Date</span>: {dateConverter(data.launch_date_local)}</div>
+              <div><span id="label-text-font">Launch Site Name</span>: {data.launch_site.site_name}</div>
+              {data.launch_success === true && <div id="label-text-font">Launch was successful</div>}
+              {data.launch_success === false &&
+                <div>
+                  <span id="label-text-font">Launch Failure Details</span>
+                  {data.launch_success === false && <div><span id="label-text-font">Reason</span>: {data.launch_failure_details?.reason}</div>}
+                  {data.launch_success === false && <div><span id="label-text-font">Altitude</span>: {data.launch_failure_details?.altitude}</div>}
+                  {data.launch_success === false && <div><span id="label-text-font">Time</span>: {data.launch_failure_details?.time}</div>}
+                </div>}
+            </div>
+          ))}
         </div>
-        </div>
-      </>
-    )
-  }
-  
-  export default FavoritesList
+      </div>
+    </>
+  )
+}
+
+export default FavoritesList;
